e751bc0c385481789497807ea95db1817eb955f1
RNJey <miranyuliastuti@gmail.com>
2026-05-12 01:48:43 +0700

    update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://pyhub-frontend.vercel.app'],
     credentials: true
 }));
 
